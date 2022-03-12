4ecef591f082e430a26e15221fe9e0a2c6748ba2
Jason Wilson <jasonstimphil@gmail.com>
2022-03-11 21:10:34 -0500

    added problemThree

--- js/app.js
+++ js/app.js
@@ -8,7 +8,7 @@ Write a function called sum() that takes in two numbers as arguments and then re
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSum() function below and check the console to see if the test passes.*/
 
 // Write your code here
-function sum(a, b) { //eslint-disable-line
+function sum(a, b,) { //eslint-disable-line
   let numberSum = a + b;
   let str = `The sum of ${a} and ${b} is ${numberSum}.`;
 
@@ -17,7 +17,7 @@ function sum(a, b) { //eslint-disable-line
 
 
 // Here is the test for sum(); uncomment it to run it
-testSum(4, 7);
+testSum(4, 7,);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -55,11 +55,17 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
-
+  let sumOne = sum(a,b)[0];
+  let totalSum = sum(sumOne, c)[0];
+  let mulOne = multiply(a,b)[0];
+  let totalMul = multiply(mulOne, c)[0];
+  let str3 = `${a} and ${b} and ${c} sum to ${totalSum}.`;
+  let str4 = `The product of ${a} and ${b} and ${c} is ${totalMul}.`;
+  return [totalSum, totalMul, str3, str4];
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
